92587622424c26a2254832ebc9d5fe78d561cb5f
Sosoh1705 <0000113091432xsp@al.educacao.sp.gov.br>
2024-09-05 15:50:13 -0300

    correcoes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const avanca = document.querySelectorAll (' .btn-proximo');
 
 avanca.forEach (button => {
-   button.addEventListener ('click'), function(){
+   button.addEventListener ('click', function(){
        const atual = document.querySelector('.ativo')
-       const proximoPasso = 'passo-' + this.geTattribute('data-proximo');
+       const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
        atual.classList.remove('ativo');
-       document.getElementById(propximoPasso).classList.add('ativo');
+       document.getElementById(proximoPasso).classList.add('ativo');
        
-
-}     
+   })
+} )
